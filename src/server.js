8473de8622406e67f82ca1b00a476663b8688af6
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2024-12-12 14:26:23 +0200

    add err and not found

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 // import { logger } from './middlewares/logger.js';
 
 import { env } from './utils/env.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -20,18 +22,9 @@ export const startServer = () => {
     });
   });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
