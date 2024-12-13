51f5b648ed7926ec3111367c7d3c311d00d225a6
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-12-13 14:52:42 +0100

    files upd

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,7 +2,7 @@ import {  Router } from "express";
 import {validateBody} from '../middlewares/validateBody.js';
 import { loginUserSchema, registrUserSchema, requestresetPasswordSchema } from '../validation/auth.js';
 import { createUserController, logInUserController, logOutUserController,  requestResetEmailController } from "../controllers/auth.js";
-import { ctrlWrapper } from "../middlewares/ctrlWrapper.js";
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 
 const router = Router();
