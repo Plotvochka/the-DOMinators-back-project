531fb3f3220128219d6cf7e93ede25a057f77f74
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2024-12-13 16:12:30 +0200

    some change

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,20 +1,38 @@
-import {  Router } from "express";
-import {validateBody} from '../middlewares/validateBody.js';
-import { loginUserSchema, registrUserSchema, requestresetPasswordSchema } from '../validation/auth.js';
-import { createUserController, logInUserController, logOutUserController,  requestResetEmailController } from "../controllers/auth.js";
-import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-
+import { Router } from 'express';
+import validateBody from '../middlewares/validateBody.js';
+import {
+  loginUserSchema,
+  registrUserSchema,
+  requestresetPasswordSchema,
+} from '../validation/auth.js';
+import {
+  createUserController,
+  logInUserController,
+  logOutUserController,
+  requestResetEmailController,
+} from '../controllers/auth.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
-router.post('/singup', validateBody(registrUserSchema), ctrlWrapper(createUserController));
+router.post(
+  '/singup',
+  validateBody(registrUserSchema),
+  ctrlWrapper(createUserController),
+);
 
-router.post('/singin', validateBody(loginUserSchema), ctrlWrapper(logInUserController));
+router.post(
+  '/singin',
+  validateBody(loginUserSchema),
+  ctrlWrapper(logInUserController),
+);
 
 router.post('/logout', ctrlWrapper(logOutUserController));
 
-router.post('/request-reset-pwd', validateBody(requestresetPasswordSchema), ctrlWrapper(requestResetEmailController));
-
-
+router.post(
+  '/request-reset-pwd',
+  validateBody(requestresetPasswordSchema),
+  ctrlWrapper(requestResetEmailController),
+);
 
 export default router;
