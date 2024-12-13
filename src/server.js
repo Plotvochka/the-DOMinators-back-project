fa2d13cfe3f0784685b498b85c6ccb5d8eab7a20
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2024-12-13 16:55:07 +0200

    upload

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import router from './routers/auth.js';
 
-
 import userRouter from './routers/user.js';
 
 const PORT = Number(env('PORT', '3000'));
@@ -20,11 +19,9 @@ export const startServer = () => {
 
   // app.use(logger);
 
-
   app.use(cookieParser());
 
   app.use(router);
-
 
   app.get('/', (req, res) => {
     res.json({
@@ -32,17 +29,12 @@ export const startServer = () => {
     });
   });
 
-
-
-
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-
   });
 
-  app.use(router); 
-  
+  app.use(router);
+
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
